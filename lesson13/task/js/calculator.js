9efa10bef87b061d53d9dd7bc6623d1b352cfbc8
Roman <mandrykaroman01@gmail.com>
2023-09-13 10:46:06 +0200

    test

--- lesson13/task/js/calculator.js
+++ lesson13/task/js/calculator.js
@@ -1,8 +1,9 @@
  export const getSquaredArray = arr => {
-     arr.filter(num => num * num);
+     return arr.map(num => num * num);
  }
+
  export const getOddNumbers = arr => {
-     arr.filter(num => num % 2 === 0);
+     return arr.filter(num => num % 2 === 1);
  }
 
  export default (a, b) => a + b;
--- lesson13/task/js/test/calculator.test.js
+++ lesson13/task/js/test/calculator.test.js
@@ -8,6 +8,7 @@ it('should get squered arr', () => {
     expect(result).toEqual([1, 4, 9]);
 })
 
+
 it('should keep odd', () => {
     const result = getOddNumbers([1, 2, 3, 4, 5])
     expect(result).toEqual([1, 3, 5]);
