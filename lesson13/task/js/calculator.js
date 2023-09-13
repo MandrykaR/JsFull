5bc74b78de3d13d398bdd38098cab8a43c9704f9
Roman <mandrykaroman01@gmail.com>
2023-09-13 10:23:28 +0200

    first commit

---
+++ lesson13/task/js/calculator.js
@@ -0,0 +1,8 @@
+ export const getSquaredArray = arr => {
+     arr.filter(num => num * num);
+ }
+ export const getOddNumbers = arr => {
+     arr.filter(num => num % 2 === 0);
+ }
+
+ export default (a, b) => a + b;
