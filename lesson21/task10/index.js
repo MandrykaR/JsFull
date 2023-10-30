527a971d50a2f662b8d2dc66bd4e4624bc64d916
Roman <mandrykaroman01@gmail.com>
2023-10-30 20:50:08 +0100

    task10

---
+++ lesson21/task10/index.js
@@ -0,0 +1,14 @@
+export const finishFrom = () => {
+    const loginForm = document.querySelector('.login-form');
+
+    const loginInput = document.createElement('input');
+    loginInput.setAttribute('type', 'text');
+    loginInput.setAttribute('name', 'login');
+
+    const passwordInput = loginForm.querySelector('input[name="password"]');
+    passwordInput.setAttribute('type', 'password')
+
+    loginForm.prepend(loginInput);
+}
+
+finishFrom();
