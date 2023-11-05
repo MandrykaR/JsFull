d67b11ca0e94c16f71d0d2afc4a3d08393977beb
Roman <mandrykaroman01@gmail.com>
2023-11-05 16:35:45 +0100

    task1Form

---
+++ lesson23/TASK1/index.js
@@ -0,0 +1,54 @@
+const emailInputElem = document.querySelector('#email');
+const passwordInputElem = document.querySelector('#password');
+
+const emailErrorElem = document.querySelector('.error-text_email');
+const passwordErrorElem = document.querySelector('.error-text_password');
+
+const isRequired = value => value
+    ? undefined
+    : 'Required';
+
+const isEmail = value => value.includes('@')
+    ? undefined
+    : 'Should be an email';
+
+
+const validatorsByField = {
+    email: [isRequired, isEmail],
+    password: [isRequired]
+}
+
+const validate = (fieldName, value) => {
+    const validators = validatorsByField[fieldName];
+    return validators
+        .map(validator => validator(value))
+        .filter(errorText => errorText)
+        .join(', ')
+}
+
+const onEmailChange = e => {
+    const errorText = validate('email', e.target.value)
+    emailErrorElem.textContent = errorText;
+}
+
+
+
+const onPasswordChange = e => {
+    const errorText = validate('password', e.target.value)
+    passwordErrorElem.textContent = errorText;
+}
+
+emailInputElem.addEventListener('input', onEmailChange);
+passwordInputElem.addEventListener('input', onPasswordChange);
+
+const formElem = document.querySelector('.login-form');
+
+const onFormSuibmit = e => {
+    e.preventDefault();
+    const formData = [...new FormData(formElem)]
+        .reduce((acc, [field, value]) => ({ ...acc, [field]: value }), {})
+
+    alert(JSON.stringify(formData));
+}
+
+formElem.addEventListener('submit', onFormSuibmit);
