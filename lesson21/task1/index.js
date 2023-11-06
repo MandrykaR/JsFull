63f5c9e427e83d281840a20bc16c088d986535e0
Roman <mandrykaroman01@gmail.com>
2023-11-06 20:54:50 +0100

    todo

--- lesson21/task1/index.js
+++ lesson21/task1/index.js
@@ -1,34 +1,63 @@
-'use strict';
-
-export const tasks = [
-    { text: 'Buy milk', done: false },
-    { text: 'Pick up Tom from airport', done: false },
-    { text: 'Visit party', done: false },
-    { text: 'Visit doctor', done: true },
-    { text: 'Buy meat', done: true },
+const tasks = [
+    { id: 1, text: 'Buy milk', done: false },
+    { id: 2, text: 'Pick up Tom from airport', done: false },
+    { id: 3, text: 'Visit party', done: false },
+    { id: 4, text: 'Visit doctor', done: true },
+    { id: 5, text: 'Buy meat', done: true },
 ];
 
+const listElem = document.querySelector('.list');
+const inputElem = document.querySelector('.task-input');
+const buttonCreate = document.querySelector('.create-task-btn');
 
+const generateId = () => {
+    const maxId = Math.max(...tasks.map(task => task.id), 0);
+    return maxId + 1;
+}
 
-const renderTasks = tasksList => {
-    const listElem = document.querySelector('.list');
+const createdTask = () => {
+    const taskText = inputElem.value;
 
-    const listItemsElems = tasksList
-        .sort((a, b) => a.done - b.done)
-        .map(({ text, done }) => {
-            const listItemElem = document.createElement('li');
-            listItemElem.classList.add('list__item');
-            const checkboxElem = document.createElement('input');
-            checkboxElem.setAttribute('type', 'checkbox');
-            checkboxElem.checked = done;
-            done ? listItemElem.classList.add('list__item_done') : null;
-            checkboxElem.classList.add('list__item-checkbox')
-            listItemElem.append(checkboxElem, text);
+    if (taskText) {
+        const newTask = { id: generateId(), text: taskText, done: false };
+        tasks.push(newTask);
+        renderTasks();
+        inputElem.value = '';
+    }
+}
+buttonCreate.addEventListener('click', createdTask);
 
-            return listItemElem;
-        })
+const renderTasks = () => {
+    listElem.innerHTML = '';
 
-    listElem.append(...listItemsElems);
+    tasks.sort((a, b) => a.done - b.done).forEach(({ id, text, done }) => {
+        const listItemElem = document.createElement('li');
+        listItemElem.classList.add('list__item');
+
+        const checkbox = document.createElement('input');
+        checkbox.setAttribute('type', 'checkbox');
+        checkbox.checked = done;
+        checkbox.classList.add('list__item-checkbox');
+        checkbox.setAttribute('data-task-id', id);
+        if (done) {
+            listItemElem.classList.add('list__item_done');
+        }
+        checkbox.addEventListener('change', () => {
+            const taskId = parseInt(checkbox.getAttribute('data-task-id'));
+            const task = tasks.find(task => task.id === taskId);
+            if (task) {
+                task.done = checkbox.checked;
+                renderTasks();
+            }
+        });
+
+        const taskTextElem = document.createElement('span');
+        taskTextElem.textContent = text;
+
+        listItemElem.append(checkbox, taskTextElem);
+        listElem.appendChild(listItemElem);
+    })
 };
 
-renderTasks(tasks);
+
+renderTasks();
