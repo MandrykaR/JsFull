7b9ff9cac175aa2116ef5afd3ba8afe747260d1a
Roman <mandrykaroman01@gmail.com>
2023-11-12 15:59:05 +0100

    task-BirthDate

---
+++ lesson24/task4/index.js
@@ -0,0 +1,25 @@
+let students = [
+    { name: 'Tom', birthDate: '12/3/2010' },
+    { name: 'Ben', birthDate: '05/7/2001' },
+    { name: 'Sam', birthDate: '05/15/2022' }
+];
+
+const studentsBirthDays = (students) => {
+    const studentsCopy = [...students];
+
+    studentsCopy.sort((a, b) => new Date(a.birthDate) - new Date(b.birthDate));
+
+    let groupedStudents = {};
+
+    studentsCopy.forEach(student => {
+        let birthDate = new Date(student.birthDate);
+        let month = birthDate.toLocaleString('en-US', { month: 'short' });
+
+        groupedStudents[month] = groupedStudents[month] ? groupedStudents[month].concat(student.name) : [student.name];
+    });
+
+    return groupedStudents;
+}
+
+let groupedByBirthdays = studentsBirthDays(students);
+console.log(groupedByBirthdays);
