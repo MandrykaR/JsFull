e522875a6819874353b17f17a3a057e37a13adae
Roman <mandrykaroman01@gmail.com>
2023-11-12 11:59:58 +0100

    task1

--- lesson24/task1/index.js
+++ lesson24/task1/index.js
@@ -1,15 +1,16 @@
-export const dayOfWeek = (date, days) => {
-    const week = ['Mo', 'Tu', 'We', 'Th', 'Fr', 'St', 'Su'];
-    const day = new Date(date).getDate();
-    const dateInFuture = new Date(date).setDate(day + days);
+const week = ['Mo', 'Tu', 'We', 'Th', 'Fr', 'St', 'Su'];
 
-    const result = week[new Date(dateInFuture).getDay()];
-    console.log(result);
-    return result;
+
+const dayOfWeek = (date, days) => {
+    let resultDay = new Date(date);
+    resultDay.setDate(resultDay.getDate() + days);
+
+    let dayOfWeekIndex = resultDay.getDay();
+
+    return week[dayOfWeekIndex]
 }
 
-dayOfWeek(new Date(2023, 10, 12), 3);
-
+console.log(dayOfWeek(new Date(2023, 10, 12), 1));
 
 
 
