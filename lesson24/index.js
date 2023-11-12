1d692c8e3e1affd41b8b34785dcc1af432d6d580
Roman <mandrykaroman01@gmail.com>
2023-11-12 11:26:21 +0100

    date

---
+++ lesson24/index.js
@@ -0,0 +1,43 @@
+
+const week = ['Mo', 'Tu', 'We', 'Th', 'Fr', 'St', 'Su'];
+
+const dayOfWeek = (date, days) => {
+    const day = new Date(date).getDate();
+    const dateInFuture = new Date(date).setDate(day + days);
+
+    return week[new Date(dateInFuture).getDay()];
+}
+
+const result = dayOfWeek(new Date(2023, 10, 12), 0)
+console.log(result);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
