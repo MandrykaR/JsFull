9b8bea98fe0790e719dc11c67541840e32d735ee
Roman <mandrykaroman01@gmail.com>
2023-11-12 15:17:13 +0100

    task2

--- lesson24/task1/index.js
+++ lesson24/task1/index.js
@@ -1,6 +1,5 @@
 const week = ['Mo', 'Tu', 'We', 'Th', 'Fr', 'St', 'Su'];
 
-
 export const dayOfWeek = (date, days) => {
     let resultDay = new Date(date);
     resultDay.setDate(resultDay.getDate() + days);
---
+++ lesson24/task2/index.js
@@ -0,0 +1,6 @@
+const getGreenwichTime = (date) => {
+    return date.toISOString().substr(11, 5)
+}
+
+let inputDate = new Date();
+getGreenwichTime(inputDate);
