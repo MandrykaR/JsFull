06923be303fcb82fe6928876c691a09c42ab3997
Roman <mandrykaroman01@gmail.com>
2023-11-12 11:28:57 +0100

    export Date

--- lesson24/task1/index.js
+++ lesson24/task1/index.js
@@ -1,7 +1,7 @@
 
-export const week = ['Mo', 'Tu', 'We', 'Th', 'Fr', 'St', 'Su'];
+const week = ['Mo', 'Tu', 'We', 'Th', 'Fr', 'St', 'Su'];
 
-const dayOfWeek = (date, days) => {
+export const dayOfWeek = (date, days) => {
     const day = new Date(date).getDate();
     const dateInFuture = new Date(date).setDate(day + days);
 
