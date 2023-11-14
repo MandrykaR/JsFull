8a3fab4ed38fdb318ee098ee9d4289420fa674b3
Roman <mandrykaroman01@gmail.com>
2023-11-14 19:36:58 +0100

    taskRotate

--- lesson24/task1/index.js
+++ lesson24/task1/index.js
@@ -1,12 +1,21 @@
-const week = ['Su', 'Mo', 'We', 'Th', 'Fr', 'St'];
+const week = ['Mo', 'Tu', 'We', 'Th', 'Fr', 'St', 'Su'];
+
+const rotateWeek = (arr, positions) => {
+    const rotatedWeek = [...arr];
+    const shifted = rotatedWeek.splice(0, positions % arr.length);
+    rotatedWeek.push(...shifted);
+
+    return rotatedWeek;
+}
 
 export const dayOfWeek = (date, days) => {
     let resultDay = new Date(date);
     resultDay.setDate(resultDay.getDate() + days);
 
     let dayOfWeekIndex = resultDay.getDay();
+    const rotatedWeek = rotateWeek(week, 1);
 
-    return week[dayOfWeekIndex]
+    return rotatedWeek[dayOfWeekIndex];
 }
 
 
