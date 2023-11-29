943c9b47351f59f533a4e49df4b2049588efe51f
Roman <mandrykaroman01@gmail.com>
2023-11-29 21:27:18 +0100

    loadImg

--- lesson29/task1/index.js
+++ lesson29/task1/index.js
@@ -15,8 +15,6 @@ export const addImage = (imgSrc, callback) => {
     imgElem.addEventListener('error', () => callback('Image load is failed'))
 }
 
-const imgSrc = 'https://p.bigstockphoto.com/GeFvQkBbSLaMdpKXF1Zv_bigstock-Aerial-View-Of-Blue-Lakes-And--227291596.jpg';
-
 
 const onImageLoaded = (error, imgElem) => {
     if (error) {
