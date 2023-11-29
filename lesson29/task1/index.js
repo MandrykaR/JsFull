537ba0ecdb450d38179160642cad6b4c89996b90
Roman <mandrykaroman01@gmail.com>
2023-11-29 21:24:04 +0100

    load

--- lesson29/task1/index.js
+++ lesson29/task1/index.js
@@ -30,5 +30,5 @@ const onImageLoaded = (error, imgElem) => {
     sizeElem.textContent = `${width} x ${height}`;
 };
 
-addImage(imgSrc, onImageLoaded);
+addImage('https://p.bigstockphoto.com/GeFvQkBbSLaMdpKXF1Zv_bigstock-Aerial-View-Of-Blue-Lakes-And--227291596.jpg', onImageLoaded);
 
