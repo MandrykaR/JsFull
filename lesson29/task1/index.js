fb118b9e61e3d06ab8882cd61c7a645965706b67
Roman <mandrykaroman01@gmail.com>
2023-12-03 11:56:39 +0100

    load

--- lesson29/task1/index.js
+++ lesson29/task1/index.js
@@ -28,7 +28,7 @@
 //     sizeElem.textContent = `${width} x ${height}`;
 // };
 
-const addImage = (imgSrc, callback) => {
+export const addImage = (imgSrc, callback) => {
     const imgElem = document.createElement('img');
     imgElem.setAttribute('alt', 'My Photo');
     imgElem.src = imgSrc;
