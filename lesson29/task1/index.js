a28e845e8a7a65fb6371eca54bb66c3793264a15
Roman <mandrykaroman01@gmail.com>
2023-12-03 11:56:24 +0100

    LOAD

--- lesson29/task1/index.js
+++ lesson29/task1/index.js
@@ -1,4 +1,34 @@
-export const addImage = (imgSrc, callback) => {
+// export const addImage = (imgSrc, callback) => {
+//     const imgElem = document.createElement('img');
+//     imgElem.setAttribute('alt', 'My Photo');
+//     imgElem.src = imgSrc;
+//     const containerElem = document.querySelector('.page');
+//     containerElem.append(imgElem);
+
+//     const onImageLoaded = () => {
+//         const { width, height } = imgElem;
+//         callback(null, { width, height });
+//     }
+
+//     imgElem.addEventListener('load', onImageLoaded);
+
+//     imgElem.addEventListener('error', () => callback('Image load is failed'))
+// }
+
+
+// const handleImageCallback = (error, dimensions) => {
+//     if (error) {
+//         console.log(error);
+//         return;
+//     }
+
+//     const { width, height } = dimensions;
+//     const sizeElem = document.querySelector('.image-size');
+
+//     sizeElem.textContent = `${width} x ${height}`;
+// };
+
+const addImage = (imgSrc, callback) => {
     const imgElem = document.createElement('img');
     imgElem.setAttribute('alt', 'My Photo');
     imgElem.src = imgSrc;
@@ -6,24 +36,28 @@ export const addImage = (imgSrc, callback) => {
     containerElem.append(imgElem);
 
     const onImageLoaded = () => {
-        const { width, height } = imgElem;
-        callback(null, { width, height });
-    }
+        callback(null, imgElem);
+    };
+
+    const onImageError = () => {
+        callback('Image load is failed');
+    };
 
     imgElem.addEventListener('load', onImageLoaded);
+    imgElem.addEventListener('error', onImageError);
+};
 
-    imgElem.addEventListener('error', () => callback('Image load is failed'))
-}
-
-
-const handleImageCallback = (error, dimensions) => {
+const onImageLoaded = (error, imgElem) => {
     if (error) {
         console.log(error);
         return;
     }
 
-    const { width, height } = dimensions;
+    const { width, height } = imgElem;
     const sizeElem = document.querySelector('.image-size');
 
     sizeElem.textContent = `${width} x ${height}`;
 };
+
+
+addImage('https://p.bigstockphoto.com/GeFvQkBbSLaMdpKXF1Zv_bigstock-Aerial-View-Of-Blue-Lakes-And--227291596.jpg', onImageLoaded);
