5d569d577dd6f789c1973f73efcb303b7340158e
Roman <mandrykaroman01@gmail.com>
2023-12-03 12:52:48 +0100

    Pinger

--- lesson29/task2/index.js
+++ lesson29/task2/index.js
@@ -2,7 +2,7 @@ export const pinger = (count, period) => {
     let i = count;
     console.log('Ping');
     const interv = setInterval(() => {
-        if (i-- > 0) {
+        if (--i > 0) {
             console.log('Ping');
         } else {
             clearInterval(interv);
