7fdcfdd4313df6c9c91511e49864d0bc2c640f22
Roman <mandrykaroman01@gmail.com>
2023-12-03 11:45:59 +0100

    load img

--- lesson29/task1/index.js
+++ lesson29/task1/index.js
@@ -26,7 +26,4 @@ const onImageLoaded = (error, imgElem) => {
     const sizeElem = document.querySelector('.image-size');
 
     sizeElem.textContent = `${width} x ${height}`;
-};
-
-addImage('https://p.bigstockphoto.com/GeFvQkBbSLaMdpKXF1Zv_bigstock-Aerial-View-Of-Blue-Lakes-And--227291596.jpg', onImageLoaded);
-
+};  
