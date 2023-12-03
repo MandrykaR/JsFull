01c0d83e0581b18a1e0725a2df17713cd2003055
Roman <mandrykaroman01@gmail.com>
2023-12-03 12:43:30 +0100

    pinger

--- lesson29/task2/index.js
+++ lesson29/task2/index.js
@@ -9,3 +9,6 @@ export const pinger = (count, period) => {
         }
     }, period);
 };
+
+pinger(5, 100); // makes 5 writes with 100 ms interval
+pinger(7, 150); // makes 7 writes with 1500 ms interval
