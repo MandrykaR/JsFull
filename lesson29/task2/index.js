0f6d19bcfaf1de1955e4a5b1920526e7cbe3b9e6
Roman <mandrykaroman01@gmail.com>
2023-12-03 12:47:43 +0100

    interval

--- lesson29/task2/index.js
+++ lesson29/task2/index.js
@@ -1,14 +1,11 @@
 export const pinger = (count, period) => {
     let i = count;
-    console.log('Ping');
     const interv = setInterval(() => {
-        if (i-- > 0) {
+        if (i > 0) {
             console.log('Ping');
+            i--;
         } else {
             clearInterval(interv);
         }
     }, period);
 };
-
-pinger(5, 100); // makes 5 writes with 100 ms interval
-pinger(7, 150); // makes 7 writes with 1500 ms interval
