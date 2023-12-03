35eba0dd4b87ef8bd792c613dc5fe6319c5ba385
Roman <mandrykaroman01@gmail.com>
2023-12-03 11:58:48 +0100

    final

--- lesson29/task1/index.js
+++ lesson29/task1/index.js
@@ -1,33 +1,3 @@
-// export const addImage = (imgSrc, callback) => {
-//     const imgElem = document.createElement('img');
-//     imgElem.setAttribute('alt', 'My Photo');
-//     imgElem.src = imgSrc;
-//     const containerElem = document.querySelector('.page');
-//     containerElem.append(imgElem);
-
-//     const onImageLoaded = () => {
-//         const { width, height } = imgElem;
-//         callback(null, { width, height });
-//     }
-
-//     imgElem.addEventListener('load', onImageLoaded);
-
-//     imgElem.addEventListener('error', () => callback('Image load is failed'))
-// }
-
-
-// const handleImageCallback = (error, dimensions) => {
-//     if (error) {
-//         console.log(error);
-//         return;
-//     }
-
-//     const { width, height } = dimensions;
-//     const sizeElem = document.querySelector('.image-size');
-
-//     sizeElem.textContent = `${width} x ${height}`;
-// };
-
 export const addImage = (imgSrc, callback) => {
     const imgElem = document.createElement('img');
     imgElem.setAttribute('alt', 'My Photo');
