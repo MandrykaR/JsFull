d4a5bbdbc68bf70a2dcce4adc453497a6e70d9c5
Roman <mandrykaroman01@gmail.com>
2023-12-03 12:41:02 +0100

    interv

--- lesson29/task2/index.js
+++ lesson29/task2/index.js
@@ -6,4 +6,5 @@ export const pinger = (count, period) => {
     }, period)
 };
 
-pinger(5, 1000); 
+pinger(5, 100); // makes 5 writes with 100 ms interval
+pinger(7, 150); // makes 7 writes with 1500 ms interval
