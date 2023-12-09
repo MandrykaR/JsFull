f44a62de1f20b58dc1424eff9d32b3e0fcf07d33
Roman <mandrykaroman01@gmail.com>
2023-12-09 10:47:57 +0100

    Promise handlers

--- lesson31/task1/index.js
+++ lesson31/task1/index.js
@@ -1,35 +1,30 @@
 
 export const requestUserData = userId => {
-    const promis = new Promise((resolve, reject) => {
-        setTimeout(() => {
-            if (userId === 'broken') {
+    const request = new Promise((resolve, reject) => {
+        if (userId === 'broken') {
+            setTimeout(() => {
                 reject(new Error('User not found'))
-            } else {
-                const userData = {
-                    name: 'John',
+            }, 500)
+        } else {
+            setTimeout(() => {
+                resolve({
+                    ame: 'John',
                     age: 17,
                     userId: `user id ${userId}`,
                     email: `userid${userId}@example.com`
-                };
-                resolve(userData);
-            }
-        }, 1000)
-    });
-    return promis;
-};
+                })
+            }, 1000)
+        }
+    })
+    return request;
+}
+
 
 requestUserData('777')
-    .then((data) => {
-        console.log('User Data:', data);
-    })
-    .catch((error) => {
-        console.log('Error:', error.message);
-    });
+    .then(data => console.log('User Data:', data))
+    .catch(error => console.log('Error:', error.message))
+    .finally(() => console.log('finally'))
 
 requestUserData('broken')
-    .then((data) => {
-        console.log('User Data:', data);
-    })
-    .catch((error) => {
-        console.log('Error:', error.message);
-    });
+    .then(data => console.log('User Data:', data))
+    .catch(error => console.log('Error:', error.message));
