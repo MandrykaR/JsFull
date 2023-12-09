8a4b38fc2e9e6eb5065b727366a39a81440988fd
Roman <mandrykaroman01@gmail.com>
2023-12-09 10:54:04 +0100

    handlers

--- lesson31/task1/index.js
+++ lesson31/task1/index.js
@@ -10,7 +10,7 @@ export const requestUserData = userId => {
                 resolve({
                     name: 'John',
                     age: 17,
-                    userId: `user id ${userId}`,
+                    userId: `userid ${userId}`,
                     email: `userid${userId}@example.com`
                 })
             }, 1000)
@@ -20,11 +20,20 @@ export const requestUserData = userId => {
 }
 
 
-requestUserData('777')
-    .then(data => console.log('User Data:', data))
-    .catch(error => console.log('Error:', error.message))
-    .finally(() => console.log('finally'))
+// requestUserData('777')
+//     .then(data => console.log('User Data:', data))
+//     .catch(error => console.log('Error:', error.message))
+//     .finally(() => console.log('finally'))
 
-requestUserData('broken')
-    .then(data => console.log('User Data:', data))
-    .catch(error => console.log('Error:', error.message));
+// requestUserData('broken')
+//     .then(data => console.log('User Data:', data))
+//     .catch(error => console.log('Error:', error.message));
+
+requestUserData('777')
+    .catch(error => {
+        console.log(error);
+        throw new Error('Error');
+    })
+    .then(data => console.log(data))
+    .catch(err => console.log(err))
+    .finally(() => console.log('finally'))
