f04593ca9f7fcad36e8328dcae89293ee24cd21e
Roman <mandrykaroman01@gmail.com>
2023-12-11 18:16:18 +0100

    prom

--- lesson31/task2/index.js
+++ lesson31/task2/index.js
@@ -4,17 +4,23 @@ export const asyncCalculator = (numbr) => new Promise((resolve) => {
         resolve(numbr);
     }, 500);
 })
-    .then(value => new Promise((resolve) => {
-        setTimeout(() => {
-            const result = value * value;
-            console.log(`Squared value: ${result}`);
-            resolve(result);
-        }, 500);
-    }))
-    .then(value => {
-        const result = value * 2;
-        console.log(`Double value: ${result}`);
-        return result;
-    });
+    .then(value => Promise.all([
+        new Promise((resolve) => {
+            setTimeout(() => {
+                const result = value * value;
+                console.log(`Squared value: ${result}`);
+                resolve(result);
+            }, 500);
+        }),
+        new Promise((resolve) => {
+            setTimeout(() => {
+                const result = value * value * 2;
+                console.log(`Double squared value: ${result}`);
+                resolve(result);
+            }, 0);
+        })
+    ]))
+    
+asyncCalculator(4)
+    .then(value => console.log(value))
 
-asyncCalculator(4);
