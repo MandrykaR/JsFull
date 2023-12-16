cb2b4e9bfd157c2da1d8cafc9e82aaded9326a5a
Roman <mandrykaroman01@gmail.com>
2023-12-16 21:21:52 +0100

    asyncNumber

--- lesson32/task1/index.js
+++ lesson32/task1/index.js
@@ -8,18 +8,14 @@ const getValueWithDelay = (value, delay) => new Promise(resolve => {
 const asyncNumber1 = getValueWithDelay(56, 1000)
 const asyncNumber2 = getValueWithDelay(undefined, 2000)
 const asyncNumber3 = getValueWithDelay(3, 3000)
-const asyncNumber4 = getValueWithDelay(23, 5000)
 
 const getSum = numbers =>
     numbers
         .filter(value => !isNaN(value))
         .reduce((acc, num) => acc + Number(num), 0)
 
-const asyncSum = (...asyncNumbers) => {
+export const asyncSum = (...asyncNumbers) => {
     return Promise.all(asyncNumbers)
         .then(numbers => getSum(numbers))
-        .catch(() => Promise.reject(new Error('Can\'t calculator')))
+        .catch(() => Promise.reject(new Error('Can\'t calculate')))
 }
-
-asyncSum(asyncNumber1, Promise.reject(new Error('err')), asyncNumber3, asyncNumber4)
-    .then(result => console.log(result));
