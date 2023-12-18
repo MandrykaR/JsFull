994edc6c5981f01d2cfd43e8bd5cdf857b8fdb39
Roman <mandrykaroman01@gmail.com>
2023-12-18 21:02:41 +0100

    promise

--- lesson32/task2/index.js
+++ lesson32/task2/index.js
@@ -2,7 +2,6 @@ const getRandomNumber = (from, to) =>
     from + Math.random() * (to - from);
 
 const request = url => new Promise(resolve => {
-
     const randomDelay = getRandomNumber(1000, 3000);
 
     setTimeout(() => {
@@ -13,22 +12,22 @@ const request = url => new Promise(resolve => {
             },
             source: url
         });
-    }, randomDelay)
+    }, randomDelay);
 });
 
 const servers = [
-    'http://server.com.us',
-    'http://server.com.eu',
-    'http://server.com.au'
-]
+    'https://server.com/us',
+    'https://server.com/eu',
+    'https://server.com/au'
+];
 
 export const getUserASAP = userId => {
-    const userUrls = servers
-        .map(serverUrl => `${serverUrl}/users/${userId}`);
+    const userUrls = servers.map(serverUrl => `${serverUrl}/${userId}`);
 
-    const requests = userUrls
-        .map(userUrl => request(userUrl));
+    const requests = userUrls.map(userUrl => request(userUrl));
 
     return Promise.race(requests);
-}
+};
 
+getUserASAP('user-id-1')
+    .then(res => console.log(res));
