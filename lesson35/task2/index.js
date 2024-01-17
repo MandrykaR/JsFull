5abddff0786e39e139088090433f5168fdcc5974
Roman <mandrykaroman01@gmail.com>
2024-01-17 20:39:42 +0100

    err

--- lesson35/task2/index.js
+++ lesson35/task2/index.js
@@ -21,7 +21,6 @@ failRequest
         console.log('onSuccess2', data);
     })
     .catch(function onError3(error) {
-        console.error('onError3', error.message); // (3)
-        throw new Error('Server error');
+        console.error('onError3', error.message);
     });
 
