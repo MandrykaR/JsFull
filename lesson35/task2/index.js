bd9d451666cf68e35d070073ef05a9b80106405d
Roman <mandrykaroman01@gmail.com>
2024-01-17 20:33:08 +0100

    prom err

--- lesson35/task2/index.js
+++ lesson35/task2/index.js
@@ -15,14 +15,13 @@ failRequest
     .catch(function onError2(error) {
         console.error('onError2', error.message);
         // (2)
-        return Promise.reject(error);
+        return Promise.reject(new Error('Error with data'));
     })
     .then(function onSuccess2(data) {
         console.log('onSuccess2', data);
     })
     .catch(function onError3(error) {
-        console.error('onError3', error.message); // (3)
-        if (error.message === 'Something went wrong') {
-            throw new Error('Server error');
-        }
+        console.error('onError3', error.message);
+        // (3)
+        throw new Error('Server error');
     });
