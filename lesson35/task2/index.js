ed148ef3953a70a9fe625da5dd03f32212488b74
Roman <mandrykaroman01@gmail.com>
2024-01-17 20:34:28 +0100

    err

--- lesson35/task2/index.js
+++ lesson35/task2/index.js
@@ -15,7 +15,7 @@ failRequest
     .catch(function onError2(error) {
         console.error('onError2', error.message);
         // (2)
-        return Promise.reject(new Error('Error with data'));
+        throw new Error('Error with data');
     })
     .then(function onSuccess2(data) {
         console.log('onSuccess2', data);
