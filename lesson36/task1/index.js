3c46b3b1b309ec901626f084386228b7f1a0e1ad
Roman <mandrykaroman01@gmail.com>
2024-01-21 19:19:34 +0100

    async

---
+++ lesson36/task1/index.js
@@ -0,0 +1,17 @@
+export const fetchUser = async userId => {
+    try {
+        const response = await fetch(`https:api.github.com/users/${userId}`);
+
+        if (!response.ok) {
+            return null
+        }
+        const userData = await response.json();
+        return userData;
+    } catch (error) {
+        throw new Error('Failed to get user data')
+    }
+};
+
+fetchUser('facebook')
+    .then(userData => console.log(userData))
+    .catch(err => alert(err.message));
