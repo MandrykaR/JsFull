8378486544f139c1b9bd2d57e2355d9d762147b3
Roman Mandryka <mandrykaroman01@gmail.com>
2024-01-24 21:01:35 +0100

    http

---
+++ lesson36/task2/index.js
@@ -0,0 +1,20 @@
+const getUsersBlogs = users => {
+   const fetchUserBlog = async userId => {
+      try {
+         const response = await fetch(`https://api.github.com/users/${userId}`);
+         if (!response.ok) {
+            throw new Error(`Error fetching blog for user ${userId}: ${response.statusText}`);
+         }
+         const userData = await response.json();
+         return userData.url;
+      } catch (error) {
+         throw new Error(error.message);
+      }
+   };
+
+   return Promise.all(users.map(fetchUserBlog));
+};
+
+// examples
+getUsersBlogs(['google', 'facebook', 'reactjs']).then(linksList => console.log(linksList));
+getUsersBlogs(['microsoft']).then(linksList => console.log(linksList));
